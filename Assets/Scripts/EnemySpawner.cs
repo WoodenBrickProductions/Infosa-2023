82d38009c9fa941cb3225aeca03e840c14ecf048
Robertas Paulauskas <r.paulauskas@protonmail.com>
2023-03-25 12:14:27 +0200

    update enemy spawner

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,13 +1,21 @@
+using System;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private EnemiesSO _enemiesContainer;
+    [SerializeField] private int _spawnAmount = 1;
+    [SerializeField] private UnityEvent onSpawnEvent;   
 
     private float _timer = 0.0f;
+    private int _spawnCounter = 0;
 
     private void Update()
     {
+        if (_spawnCounter == _spawnAmount)
+            return;
+        
         _timer += Time.deltaTime;
 
         if (_timer >= 5.0f)
@@ -24,5 +32,18 @@ public class EnemySpawner : MonoBehaviour
 
         spawnedEnemy.transform.SetParent(null);
         spawnedEnemy.transform.position = transform.position;
+
+        _spawnCounter++;
+        
+        onSpawnEvent?.Invoke();
     }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        
+        Gizmos.DrawSphere(transform.position, 1.0f);
+    }
+#endif // UNITY_EDITOR
 }
